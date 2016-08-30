d5668bc0fceab27705efade20e80a02bf68550e1
Adam Scott <adam.scott@providence.org>
2016-08-30 16:02:13 -0700

    Current working endpoint - Does not link accounts.

---
+++ Default.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" CodeBehind="default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+    
+    
+<body>
+    <form id="form1" runat="server">
+    <div style="text-align: center;">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <br /> <br />
+        <asp:Label  runat="server" Text="Welcome!" Font-Size="X-Large" />
+        <br /> <br />
+        <asp:Label  runat="server" Text="Thank you for using Providence's First Aid Alexa Skill." />
+        <br /> <br /> <br />
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <div runat="server" id="LoginDiv" >
+                    <asp:Label  runat="server" Text="Username:" />
+                    <asp:TextBox runat="server" ID="UserBox" TextMode="Email"/>
+                    <br /> <br />
+                    <asp:Label runat="server" Text="Password:" />
+                    <asp:TextBox runat="server" ID="PassBox" TextMode="Password"/>
+                </div>
+                <div runat="server" id="RegisterDiv" visible="false" >
+                    <asp:Label  runat="server" Text="Email:" />
+                    <asp:TextBox runat="server" ID="EmailBox" TextMode="Email"/>
+                    <br /> <br />
+                    <asp:Label runat="server" Text="Password:" />
+                    <asp:TextBox runat="server" ID="RegPassBox" TextMode="Password"/>
+                    <br /> <br />
+                    <asp:Label runat="server" Text="First name:" />
+                    <asp:TextBox runat="server" ID="FirstBox" TextMode="Search"/>
+                    <br /> <br />
+                    <asp:Label runat="server" Text="Last name:" />
+                    <asp:TextBox runat="server" ID="LastBox" />
+                    <br /> <br />
+                    <asp:Label runat="server" Text="Preferred name:" />
+                    <asp:TextBox runat="server" ID="PrefNameBox" />
+                    <br /> <br />
+                    <asp:Label runat="server" Text="DOB:" />
+                    <asp:TextBox runat="server" Text="MM/DD/YYYY" ID="DOBBox" Width="90px" />
+                    <br /> <br />
+                    <asp:Label runat="server" Text="SSID: ###-##-" />
+                    <asp:TextBox runat="server" Text="####" ID="TextBox1" Width="35px"/>
+                    <br /> <br />
+                    <asp:Label runat="server" Text="Gender:" />
+                    <asp:DropDownList ID="GenderDropDown" runat="server">
+                        <asp:ListItem Text="Male" />
+                        <asp:ListItem Text="Female" />
+                    </asp:DropDownList>
+                </div>
+                <br /> <br />
+                <asp:Button runat="server" Text="Login" ID="LoginButton" OnClick="LoginButton_Click" />
+                <br /> <br />
+                <asp:Button runat="server" Text="New User? Click here." ID="RegisterButton" OnClick="RegisterButton_Click" />
+                <br /> <br />
+                <asp:Label runat="server" ID="ResultLabel" />
+                <br /> <br />
+                <asp:Label runat="server" ID="URLLabel" />
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private string state;
+    private static bool login = true;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            state = Request.QueryString["state"];
+        }
+    }
+
+    protected void LoginButton_Click(object sender, EventArgs e)
+    {
+        if (login)
+        {
+            string url = "https://demo.swedishexpresscare.org/api/account/login";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            request.Credentials = new NetworkCredential("nassau.test@providence.org", "Mj!Test123");
+            request.CookieContainer = new CookieContainer();
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = "{\"Email\":\"" + UserBox.Text + "\",\"Password\":\"" + PassBox.Text + "\",\"RememberMe\":\"true\"}"; // nassau.test@providence.org, Mj!Test123
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException)
+            {
+                ResultLabel.Text = "Username and/or password is incorrect.";
+                return;
+            }
+
+            String cookieName = "", cookieValue = "";
+            foreach (Cookie cookie in response.Cookies)
+            {
+                cookieName = cookie.Name;
+                cookieValue = cookie.Value;
+            }
+
+            ResultLabel.Text = "Login was successful.";
+            URLLabel.Text = "https://pitangui.amazon.com/api/skill/link/M2E9NV9WGPLRMC#state=" + state + "&access_token=" + cookieValue + "&token_type=Bearer";
+        }
+        else
+        {
+            string url = "https://demo.swedishexpresscare.org/api/account/register";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+            //request.Credentials = new NetworkCredential("nassau.test@providence.org", "Mj!Test123");
+            request.CookieContainer = new CookieContainer();
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                string json = "{\"Email\":\"" + EmailBox.Text + "\",\"PreferredName\":\"" + PrefNameBox.Text + "\",\"Password\":\"" + RegPassBox.Text + "\"}";
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+                ResultLabel.Text = json;
+            }
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            HttpWebResponse response = null;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException)
+            {
+                ResultLabel.Text = "Could not register user.";
+                return;
+            }
+            String cookieName = "", cookieValue = "";
+            foreach (Cookie cookie in response.Cookies)
+            {
+                cookieName = cookie.Name;
+                cookieValue = cookie.Value;
+            }
+            ResultLabel.Text = "Register was successful.";
+            URLLabel.Text = "https://pitangui.amazon.com/api/skill/link/M2E9NV9WGPLRMC#state=" + state + "&access_token=" + cookieValue + "&token_type=Bearer";
+        }
+    }
+
+    protected void RegisterButton_Click(object sender, EventArgs e)
+    {
+        if (login)
+        {
+            LoginDiv.Visible = false;
+            RegisterDiv.Visible = true;
+            LoginButton.Text = "Register";
+            RegisterButton.Text = "Back to login";
+            ResultLabel.Text = "";
+            URLLabel.Text = "";
+            login = false;
+        }
+        else
+        {
+            LoginDiv.Visible = true;
+            RegisterDiv.Visible = false;
+            LoginButton.Text = "Login";
+            RegisterButton.Text = "New User? Click here.";
+            ResultLabel.Text = "";
+            URLLabel.Text = "";
+            login = true;
+        }
+    }
+}
